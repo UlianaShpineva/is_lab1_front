64d2ac7f63589b6048069da2e21acc55bb39b6cf
Uliana <uliana.shpineva@gmail.com>
2024-12-01 20:07:42 +0300

    fix null objects

--- src/component/MovieRepresentation.js
+++ src/component/MovieRepresentation.js
@@ -18,11 +18,11 @@ export default function MovieRepresentation({movie, includeMain = true}) {
                 name="Movie" keys={["id", "name", "oscarsCount", "budget", "totalBoxOffice", "mpaaRating", "length", "goldenPalmCount", "usaBoxOffice", "tagline", "genre"]} object={movie}/>}
             <MovieAttribute name="Coordinates" keys={["id", "x", "y"]} object={movie.coordinates}/>
             <MovieAttribute name="Director" keys={["id", "name", "eyeColor", "hairColor", "weight", "nationality"]} object={movie.director}/>
-            <MovieAttribute name="Director Location" keys={["id", "name", "x", "y", "z"]} object={movie.director.location}/>
-            <MovieAttribute name="Screenwriter" keys={["id", "name", "eyeColor", "hairColor", "weight", "nationality"]} object={movie.screenwriter}/>
-            <MovieAttribute name="ScreenWriter Location" keys={["id", "name", "x", "y", "z"]} object={movie.screenwriter.location}/>
-            <MovieAttribute name="Operator" keys={["id", "name", "eyeColor", "hairColor", "weight", "nationality"]} object={movie.operator}/>
-            <MovieAttribute name="Operator Location" keys={["id", "name", "x", "y", "z"]} object={movie.operator.location ?? {id: "null"}}/>
+            <MovieAttribute name="Director Location" keys={["id", "name", "x", "y", "z"]} object={movie.director.location ?? {id: "null"}}/>
+            <MovieAttribute name="Screenwriter" keys={["id", "name", "eyeColor", "hairColor", "weight", "nationality"]} object={movie.screenwriter ?? {id: "null"}}/>
+            <MovieAttribute name="ScreenWriter Location" keys={["id", "name", "x", "y", "z"]} object={movie.screenwriter != null ? (movie.screenwriter.location ?? {id: "null"}) : { id: "null" }}/>
+            <MovieAttribute name="Operator" keys={["id", "name", "eyeColor", "hairColor", "weight", "nationality"]} object={movie.operator ?? {id: "null"}}/>
+            <MovieAttribute name="Operator Location" keys={["id", "name", "x", "y", "z"]} object={movie.operator != null ? (movie.operator.location ?? {id: "null"}) : {id: "null"}}/>
         </div>
     </div>
 }
